fba128e2bb977c9b51167da7a451b3b719beef7b
vishal-kamble <vishal26785@gmail.com>
2014-12-25 18:55:39 -0500

    added footer

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-define(['angular', './home/home', 'angular-route'], function (angular) {
+define(['angular', './home/home','./results/results', 'angular-route'], function (angular) {
 
     return angular
-        .module('app', ['ngRoute', 'home'])
+        .module('app', ['ngRoute', 'home','results'])
         .config(['$routeProvider', function ($routeProvider) {
             $routeProvider.otherwise('/');
         }]);
