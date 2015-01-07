ca9fd2f654eed7cc775cab4607d1a9304770be8e
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2015-01-07 18:52:20 -0500

    added basic search support

--- src/app/search/search.ctrl.js
+++ src/app/search/search.ctrl.js
@@ -4,14 +4,42 @@ define(['angular'], function (angular) {
 
     return angular
         .module('search.ctrl', [])
-        .controller('search.ctrl', ['$scope', 'accountService', function ($scope, accountService) {
-            //quick-settings collapse
-            $scope.isCollapsed = true;
+        .controller('search.ctrl', ['$scope', 'accountService', '$http', 'config', function ($scope, accountService, $http, config) {
 
+            //search - location settings
+            $scope.cities = accountService.cities;
+            $scope.searchCity = accountService.account.location;
+            $scope.searchWithin = 'city';
+            $scope.searchRadius = 10;
+            $scope.searchLocation = 'gps';
+            $scope.updateLocation = function () {
+                accountService.changeLocation($scope.searchCity);
+                $scope.refineSearch();
+            };
+
+            //search - categories settings
+            $scope.searchCategoryAll = true;
+            $scope.searchCategoryPlayers = true;
+            $scope.searchCategoryCoaches = true;
+            $scope.searchCategoryFacilities = true;
+            $scope.searchCategorySuppliers = true;
+            $scope.searchCategoryEvents = true;
+            $scope.searchCategoryOffers = true;
+
+
+            //search - content
+            $scope.search = {};
+            $scope.refineSearch = function () {
+                $http.get(config.apiBaseURL + '/search', {params: {city: $scope.searchCity}}).then(function (data) {
+                    $scope.results = data.data;
+                });
+            };
+
+            //user
             $scope.user = accountService.account.user;
-            $scope.quickSettings = accountService.account.quickSettings;
-            $scope.isAuthenticated = accountService.isAuthenticated;
-            $scope.logout = accountService.logout;
+
+            //call refine search to fetch results
+            $scope.refineSearch();
 
         }]);
 
--- src/app/search/search.js
+++ src/app/search/search.js
@@ -6,22 +6,21 @@ define(['angular', './search.ctrl', './bar/search.bar.ctrl'], function (angular)
         .module('search', ['search.ctrl', 'search.bar.ctrl'])
         .config(['$routeProvider', function ($routeProvider) {
 
-            $routeProvider.when('/search', {
+            var searchConfig = {
                 controller: 'search.ctrl',
-                templateUrl: 'app/search/results/results.tpl.html'
-            }).when('/search/:location', {
-                controller: 'search.ctrl',
-                templateUrl: 'app/search/results/results.tpl.html'
-            }).when('/coaches/:location', {
-                controller: 'search.ctrl',
-                templateUrl: 'app/search/results/results.tpl.html'
-            }).when('/offers/:location', {
-                controller: 'search.ctrl',
-                templateUrl: 'app/search/results/results.tpl.html'
-            }).when('/events/:location', {
-                controller: 'search.ctrl',
-                templateUrl: 'app/search/results/results.tpl.html'
-            });
+                templateUrl: 'app/search/results/results.tpl.html',
+                resolve: {
+                    account: ['accountService', function (accountService) {
+                        return accountService.fetch();
+                    }]
+                }
+            };
+
+            $routeProvider.when('/search', searchConfig)
+                .when('/search/:location', searchConfig)
+                .when('/coaches/:location', searchConfig)
+                .when('/offers/:location', searchConfig)
+                .when('/events/:location', searchConfig);
 
         }]);
 
