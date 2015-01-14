2904f9a4fb33af2b7b83c97d37c4e11b25b5962c
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2015-01-13 22:45:08 -0500

    updated deployment support

--- server.js
+++ server.js
@@ -2,32 +2,22 @@ var express = require('express');
 var compress = require('compression');
 var path = require('path');
 var app = express();
-var dirPath;
 
+//dirPath
+var dirPath = path.join(__dirname, '/build');
 
+//to gzip
 app.use(compress());
 
 // Force HTTPS on Heroku
-if (process.env.NODE_ENV === 'staging') {
-
+if (process.env.NODE_ENV === 'production') {
     dirPath = path.join(__dirname, '/dist');
-    app.use(express.static(dirPath));
-    app.use('/*', function (req, res) {
-        res.sendFile(dirPath + '/index.html');
-    });
-
-    //app.use(function (req, res, next) {
-    //    var protocol = req.get('x-forwarded-proto');
-    //    protocol == 'https' ? next() : res.redirect('https://' + req.hostname + req.url);
-    //});
-
-} else {
-
-    dirPath = path.join(__dirname, '/dist');
-    app.use(express.static(dirPath));
-    app.use('/*', function (req, res) {
-        res.sendFile(dirPath + '/index.html');
-    });
 }
+
+//expose static files
+app.use(express.static(dirPath));
+app.use('/*', function (req, res) {
+    res.sendFile(dirPath + '/index.html');
+});
 
 app.listen(process.env.PORT || 9000);
